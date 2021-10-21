506c024b3d804445dca668b40e36b21d6eacd37e
Sid is Live YT <sidisliveytofficial@gmail.com>
2021-10-21 15:46:12 +0530

    Updated Packages

--- src/Structures/Tracks.js
+++ src/Structures/Tracks.js
@@ -1,4 +1,4 @@
-const { Extractor } = require('playdl-music-extractor');
+const { StreamDownloader } = require('playdl-music-extractor')
 
 class TrackGenerator {
   static async fetch(
@@ -15,33 +15,33 @@ class TrackGenerator {
     CacheLength = 0,
   ) {
     if (!Query || (Query && typeof Query === 'string')) {
-      throw Error('Invalid Song Query is Detected');
+      throw Error('Invalid Song Query is Detected')
     }
     const RawData = await TrackGenerator.#SongsFetching(
       Query,
       FetchOptions,
       extractor,
-    );
+    )
     if (
-      !RawData
-      || (RawData && !RawData.tracks)
-      || (RawData && RawData.tracks && !RawData.tracks[0])
+      !RawData ||
+      (RawData && !RawData.tracks) ||
+      (RawData && RawData.tracks && !RawData.tracks[0])
     ) {
       return {
         playlist: false,
         streamdatas: [],
         tracks: [],
-      };
+      }
     }
     const Chunks = TrackGenerator.#Track_Id_Placement(
       RawData.tracks,
       CacheLength,
-    );
+    )
     return {
       playlist: RawData.playlist,
       streamdatas: Chunks.streamdatas,
       tracks: Chunks.tracks,
-    };
+    }
   }
 
   static #UserTrackModelGen(TrackData) {
@@ -58,7 +58,7 @@ class TrackGenerator {
       likes: 0,
       is_live: false,
       dislikes: 0,
-    };
+    }
   }
 
   static async #SongsFetching(
@@ -73,30 +73,32 @@ class TrackGenerator {
     },
     extractor = 'play-dl',
   ) {
-    const RawData = (extractor.includes('youtube-dl')
-      ? await TrackGenerator.#YoutubeDLExtractor(Query)
-      : null) ?? (await Extractor(Query, FetchOptions.ExtractorStreamOptions));
-    return RawData;
+    const RawData =
+      (extractor.includes('youtube-dl')
+        ? await TrackGenerator.#YoutubeDLExtractor(Query)
+        : null) ??
+      (await StreamDownloader(Query, FetchOptions.ExtractorStreamOptions))
+    return RawData
   }
 
   static #Track_Id_Placement(Tracks, CacheLength) {
-    const StreamDatas = [];
-    const SearchTracks = [];
+    const StreamDatas = []
+    const SearchTracks = []
     for (let count = 0, len = Tracks.length; count < len; ++count) {
-      Tracks[count].Id = CacheLength + 1;
-      SearchTracks.push(TrackGenerator.#UserTrackModelGen(Tracks[count]));
-      StreamDatas.push(Tracks[count]);
+      Tracks[count].Id = CacheLength + 1
+      SearchTracks.push(TrackGenerator.#UserTrackModelGen(Tracks[count]))
+      StreamDatas.push(Tracks[count])
     }
     return {
       streamdatas: StreamDatas[0] ? StreamDatas : [],
       tracks: SearchTracks[0] ? SearchTracks : [],
-    };
+    }
   }
 
   static async #YoutubeDLExtractor(Query) {
-    const Downloader = require('video-extractor').Extractor;
-    return await Downloader(Query);
+    const { StreamDownloader } = require('video-extractor')
+    return await StreamDownloader(Query)
   }
 }
 
-module.exports = TrackGenerator;
+module.exports = TrackGenerator
