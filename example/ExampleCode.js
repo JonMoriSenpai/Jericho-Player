f113c6b34c71ec171ed7f8e7192a728272863675
Sid is Live YT <sidisliveytofficial@gmail.com>
2021-11-06 22:15:14 +0530

    chore(examples): updated Examples and docs

--- example/ExampleCode.js
+++ example/ExampleCode.js
@@ -1,4 +1,16 @@
-//const { JerichoPlayer } = require('../src/index.js')
+const { JerichoPlayer } = require('../src/index.js')
 
-import { JerichoPlayer } from '../src/index.mjs'
+//import { JerichoPlayer } from '../src/index.mjs'
 var player = new JerichoPlayer()
+
+
+//Create Queue or Fetch Queue from Cache
+const Queue = player.CreateQueue(message)
+
+//Play Command for Queue per Guild scenerio
+await Queue.play('Despacito', message.member.voice.channel, message.author)
+
+
+/**
+ * for more detailed explanation , visit -> https://github.com/SidisLiveYT/Jericho-Player-Discord-Bot
+ */
