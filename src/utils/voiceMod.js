1dfae532fb259f7f28fae38e5c7c67cfa267bb46
SidisLiveYT <sidisliveytofficial@gmail.com>
2022-07-23 03:42:25 +0530

    fix(added proper typings): added Typings , event distributors and minor methods for queue and player

--- src/utils/voiceMod.js
+++ src/utils/voiceMod.js
@@ -2,19 +2,23 @@ const {
   joinVoiceChannel,
   VoiceConnectionStatus,
   entersState,
+  getVoiceConnection,
 } = require('@discordjs/voice');
-const { voiceResolver } = require('./snowflakes');
-const player = require('../core/player');
+const { voiceResolver, guildResolver } = require('./snowflakes');
+const queue = require('../core/queue');
 
 class voiceMod {
   /**
-   *
-   * @param {player} player
+   * @constructor
+   * @param {queue} queue
+   * @param {object} options
    */
-  constructor(player) {
-    this.player = player;
-    this.discordClient = player?.discordClient;
-    this.eventEmitter = player?.eventEmitter;
+  constructor(queue, options) {
+    this.queue = queue;
+    this.player = queue?.player;
+    this.discordClient = queue?.discordClient;
+    this.eventEmitter = queue?.eventEmitter;
+    this.options = options;
   }
 
   async connect(voiceSnowflake, options) {
@@ -24,7 +28,7 @@ class voiceMod {
         voiceSnowflake,
       );
       if (!voiceChannel)
-        throw new TypeError(
+        throw new Error(
           '[ Invalid Voice Channel ] : Wrong Voice Channel Snowflake/Resolve is Detected for voiceMod.connect()',
         );
       const rawVoiceConnection = joinVoiceChannel({
@@ -44,12 +48,54 @@ class voiceMod {
         throw errorMetadata;
       }
     } catch (errorMetadata) {
-      this.eventEmitter.emitError(
-        errorMetadata,
-        ' - Provide Correct Query or Voice Channel for Connection and Audio Processing',
-        'voiceMod.connect()',
-        { voiceSnowflake, options },
-        options?.eventOptions,
+      this.eventEmitter.emitEvent(
+        'connectionError',
+        errorMetadata?.message ?? `${errorMetadata}`,
+        { queue: this.queue },
+        this.options?.eventOptions,
+      );
+      return undefined;
+    }
+  }
+
+  async disconnect(guildSnowflake, delayVoiceTimeout, options) {
+    try {
+      const guild = await guildResolver(this.discordClient, guildSnowflake);
+      if (!guild)
+        throw new Error(
+          '[ Invalid Discord Guild ] : Corrupt/Invalid Guild Data by guild Snowflake has been resolved',
+        );
+      const queue = await this.player.getQueue(guild);
+      if (queue?.packet?.audioPlayer && (queue?.playing || queue?.paused))
+        queue?.packet?.audioPlayer?.stop();
+      if (
+        queue?.packet?.__privateCaches?.audioPlayerSubscription &&
+        (queue?.playing || queue?.paused)
+      )
+        queue?.packet?.__privateCaches?.audioPlayerSubscription?.unsubscribe();
+      const voiceConnection = getVoiceConnection(guild?.id);
+      if (!voiceConnection)
+        throw new Error(
+          '[ Invalid Voice Connection ] : Corrupt/Invalid Voice Connection has been fetched',
+        );
+      if (options?.forceDestroy)
+        return delayVoiceTimeout && delayVoiceTimeout > 0
+          ? setTimeout(() => {
+            voiceConnection.destroy(true);
+          }, delayVoiceTimeout * 1000)
+          : true;
+      else
+        return delayVoiceTimeout && delayVoiceTimeout > 0
+          ? setTimeout(() => {
+            voiceConnection.disconnect();
+          }, delayVoiceTimeout * 1000)
+          : true;
+    } catch (errorMetadata) {
+      this.eventEmitter.emitEvent(
+        'connectionError',
+        errorMetadata?.message ?? `${errorMetadata}`,
+        { queue: this.queue },
+        this.options?.eventOptions,
       );
       return undefined;
     }
